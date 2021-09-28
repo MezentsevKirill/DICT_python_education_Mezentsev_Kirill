2ef248479d2690506985dedf688cad197f5269c0
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2021-09-28 10:19:29 +0300

    ChatBot 2-nd stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,7 @@
+# Stage 1
+print("Hello! My name is DICT_Bot")
+print("I was created in 2021")
+# Stage 2
+print("Please, remind me your name")
+name = input("< ")
+print("What a great name you have, " + name + "!")
