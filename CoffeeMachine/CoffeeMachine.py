db71e3e945ec5f2e697ab943b9c040ac73ad5cdc
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2021-10-23 16:10:26 +0300

    CoffeeMachine 1-st stage

---
+++ CoffeeMachine/CoffeeMachine.py
@@ -0,0 +1,4 @@
+# Stage 1
+print("Starting to make a coffee\nGrinding coffee beans\nBoiling water")
+print("Mixing boiled water with crushed coffee beans\nPouring coffee into the cup")
+print("Pouring some milk into the cup\nCoffee is ready!")
