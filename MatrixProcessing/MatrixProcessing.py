c76684fe59de858f38f4d837404a7fdc728d4c84
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2021-11-23 19:03:04 +0200

    MatrixProcessing 2-nd stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,23 +1,5 @@
 # Stage 1
-n, m = map(int, input("Enter the number of lines and columns\n> ").split())
-a = []
-for i in range(n):
-    a.append(list(map(int, input().split())))
-    for j in range(m):
-        continue
-n, m = map(int, input("Enter the number of lines and columns\n> ").split())
-b = []
-for i in range(n):
-    b.append(list(map(int, input().split())))
-    for j in range(m):
-        continue
-a_unpacked = [j for i in a for j in i]
-b_unpacked = [j for i in b for j in i]
-if len(a_unpacked) == len(b_unpacked):
-    s = []
-    for i in range(n):
-        for j in range(m):
-            s.append(a[i][j] + b[i][j])
+def draw_matrix():
     if len(s) == 4:
         print([s[0], s[1]])
         print([s[2], s[3]])
@@ -44,5 +26,40 @@ if len(a_unpacked) == len(b_unpacked):
         print([s[10], s[11], s[12], s[13], s[14]])
         print([s[15], s[16], s[17], s[18], s[19]])
         print([s[20], s[21], s[22], s[23], s[24]])
+
+
+n, m = map(int, input("Enter the number of lines and columns\n> ").split())
+a = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+    for j in range(m):
+        continue
+n, m = map(int, input("Enter the number of lines and columns\n> ").split())
+b = []
+for i in range(n):
+    b.append(list(map(int, input().split())))
+    for j in range(m):
+        continue
+a_unpacked = [j for i in a for j in i]
+b_unpacked = [j for i in b for j in i]
+if len(a_unpacked) == len(b_unpacked):
+    s = []
+    for i in range(n):
+        for j in range(m):
+            s.append(a[i][j] + b[i][j])
+    draw_matrix()
 else:
     print("ERROR")
+# Stage 2
+n, m = map(int, input("Enter the number of lines and columns\n> ").split())
+a.clear()
+s = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+    for j in range(m):
+        continue
+const = int(input())
+for i in range(n):
+    for j in range(m):
+        s.append(a[i][j] * const)
+draw_matrix()
