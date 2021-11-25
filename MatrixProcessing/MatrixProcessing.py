84afe5f5b8fa3b0f1d47a989c8a7421b2b08ab38
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2021-11-25 13:23:05 +0200

    MatrixProcessing 6-th stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,4 +1,7 @@
 # Stage 1
+# import math as math
+
+
 def draw_matrix():
     if len(s) == 4:
         print([s[0], s[1]])
@@ -84,12 +87,12 @@ for i in range(n):
     for j in range(m):
         s.append(a[i][j] * const)
 draw_matrix()
-# Stage 3,4,5
-calculate_list = [1, 2, 3, 0, 4, 5]
+# Stage 3,4,5,6
+calculate_list = [1, 2, 3, 0, 4, 5, 6]
 calculate = None
 while calculate != calculate_list[3]:
     print("1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n4. Transpose matrix\n"
-          "5. Calculate a determinant\n0. Exit")
+          "5. Calculate a determinant\n6. Inverse matrix\n0. Exit")
     calculate = int(input("Your choice: > "))
     if calculate == calculate_list[0]:
         n, m = map(int, input("Enter the number of lines and columns\n> ").split())
@@ -208,3 +211,40 @@ while calculate != calculate_list[3]:
             det = a[0][0] * (a[1][1] * a[2][2] - a[2][1] * a[1][2]) - a[0][1] * (a[1][0] * a[2][2] - a[2][0] * a[1][2])\
                   + a[0][2] * (a[1][0] * a[2][1] - a[2][0] * a[1][1])
             print("The result is:\n", det)
+    if calculate == calculate_list[6]:
+        a.clear()
+        new_a = []
+        n, m = map(int, input("Enter the number of lines and columns\n> ").split())
+        for i in range(n):
+            a.append(list(map(int, input().split())))
+            for j in range(m):
+                continue
+        if len(a) == 2:
+            det = a[0][0] * a[1][1] - a[1][0] * a[0][1]
+            if det != 0:
+                t = [a[0][0], a[1][0], a[0][1], a[1][1]]
+                e = [[a[1][1], -a[0][1]], [-a[1][0], a[0][0]]]
+                for i in range(2):
+                    for j in range(2):
+                        new_a.append((1 / det) * e[i][j])
+                print("The result is:\n", "%.2f" % new_a[0], "%.2f" % new_a[1], "\n", "%.2f" % new_a[2],
+                      "%.2f" % new_a[3])
+        if len(a) == 3:
+            det = a[0][0] * (a[1][1] * a[2][2] - a[2][1] * a[1][2]) - a[0][1] * (a[1][0] * a[2][2] - a[2][0] * a[1][2]) \
+                  + a[0][2] * (a[1][0] * a[2][1] - a[2][0] * a[1][1])
+            if det != 0:
+                t = [a[0][0], a[1][0], a[2][0], a[0][1], a[1][1], a[2][1], a[0][2], a[1][2], a[2][2]]
+                e = [[a[1][1] * a[2][2] - a[1][2] * a[2][1], -(a[0][1] * a[2][2] - a[0][2] * a[2][1])
+                     , a[0][1] * a[1][2] - a[0][2] * a[1][1]], [-(a[1][0] * a[2][2] - a[1][2] * a[2][0])
+                     , a[0][0] * a[2][2] - a[0][2] * a[2][1], -(a[0][0] * a[1][2] - a[0][2] * a[1][0])]
+                     , [a[1][0] * a[2][1] - a[1][1] * a[2][0], -(a[0][0] * a[2][1] - a[0][1] * a[2][0])
+                     , a[0][0] * a[1][1] - a[0][1] * a[1][0]]]
+                for i in range(3):
+                    for j in range(3):
+                        new_a.append((1 / det) * e[i][j])
+                print("The result is:\n", "%.2f" % new_a[0], "%.2f" % new_a[1], "%.2f" % new_a[2], "\n",
+                      "%.2f" % new_a[3], "%.2f" % new_a[4], "%.2f" % new_a[5], "\n",
+                      "%.2f" % new_a[6], "%.2f" % new_a[7], "%.2f" % new_a[8])
+
+
+
