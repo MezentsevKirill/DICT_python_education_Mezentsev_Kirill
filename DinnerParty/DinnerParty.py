04c837bd8faea990915e8d91eaa34d87b17adf3b
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2021-11-29 13:09:26 +0200

    DinnerParty 3-rd stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,4 +1,5 @@
 # Stage 1,2
+import random
 number = int(input("Enter the number of friends joining (including you):\n> "))
 d = {}
 if number <= 0:
@@ -9,10 +10,23 @@ else:
         name = input("> ")
         d.update({name: 0})
     amount = int(input("Enter the total amount:\n> "))
-    payment = amount / number
-    for d_keys in d.keys():
-        if amount % number == 0:
-            d[d_keys] = payment
-        elif amount % number > 0:
-            d[d_keys] = "%.2f" % payment
+    lucky = input("Do you want to use the "'Who is lucky?'" feature? Write Yes/No:\n> ")
+    if lucky == "No":
+        payment = amount / number
+        for d_keys in d.keys():
+            if amount % number == 0:
+                d[d_keys] = payment
+            elif amount % number > 0:
+                d[d_keys] = "%.2f" % payment
+        print("No one is going to be lucky")
+    elif lucky == "Yes":
+        payment = amount / (number - 1)
+        lucky_one = random.choice(list(d.keys()))
+        for d_keys in d.keys():
+            if amount % number == 0:
+                d[d_keys] = payment
+            elif amount % (number-1) > 0:
+                d[d_keys] = "%.2f" % payment
+            d[lucky_one] = 0
+        print(lucky_one, "is the lucky one!")
     print(d)
