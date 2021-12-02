18421b3a2259962b49009e65192bb4de26822469
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2021-12-02 10:04:15 +0200

    Removed some function

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -2,58 +2,6 @@
 a = []
 b = []
 s = []
-
-
-def draw_matrix():
-    if len(s) == 4:
-        print([s[0], s[1]])
-        print([s[2], s[3]])
-    elif len(s) == 9:
-        print([s[0], s[1], s[2]])
-        print([s[3], s[4], s[5]])
-        print([s[6], s[7], s[8]])
-    elif len(s) == 6:
-        print([s[0], s[1], s[2]])
-        print([s[3], s[4], s[5]])
-    elif len(s) == 16:
-        print([s[0], s[1], s[2], s[3]])
-        print([s[4], s[5], s[6], s[7]])
-        print([s[8], s[9], s[10], s[11]])
-        print([s[12], s[13], s[14], s[15]])
-    elif len(s) == 20:
-        print([s[0], s[1], s[2], s[3], s[4]])
-        print([s[5], s[6], s[7], s[8], s[9]])
-        print([s[10], s[11], s[12], s[13], s[14]])
-        print([s[15], s[16], s[17], s[18], s[19]])
-    elif len(s) == 25:
-        print([s[0], s[1], s[2], s[3], s[4]])
-        print([s[5], s[6], s[7], s[8], s[9]])
-        print([s[10], s[11], s[12], s[13], s[14]])
-        print([s[15], s[16], s[17], s[18], s[19]])
-        print([s[20], s[21], s[22], s[23], s[24]])
-
-
-def draw_matrix_v2():
-    if len(s) == 2:
-        print(s[0])
-        print(s[1])
-    elif len(s) == 3:
-        print(s[0])
-        print(s[1])
-        print(s[2])
-    elif len(s) == 4:
-        print(s[0])
-        print(s[1])
-        print(s[2])
-        print(s[3])
-    elif len(s) == 5:
-        print(s[0])
-        print(s[1])
-        print(s[2])
-        print(s[3])
-        print(s[4])
-
-
 # Stage 1,2,3,4,5,6
 calculate_list = [1, 2, 3, 0, 4, 5, 6]
 calculate = None
@@ -77,26 +25,31 @@ while calculate != calculate_list[3]:
         a_unpacked = [j for i in a for j in i]
         b_unpacked = [j for i in b for j in i]
         if len(a_unpacked) == len(b_unpacked):
-            s = []
-            for i in range(n):
-                for j in range(m):
-                    s.append(a[i][j] + b[i][j])
-            draw_matrix()
+            length = len(a)
+            length1 = len(b)
+            s = [[0 for j in range(length1)] for i in range(length)]
+            for i in range(length):
+                for j in range(length):
+                    s[i][j] = a[i][j] + b[i][j]
+            for elem in s:
+                print(elem)
         else:
             print("ERROR")
     if calculate == calculate_list[1]:
         n, m = map(int, input("Enter the number of lines and columns\n> ").split())
         a.clear()
-        s = []
         for i in range(n):
             a.append(list(map(int, input().split())))
             for j in range(m):
                 continue
         const = int(input())
-        for i in range(n):
-            for j in range(m):
-                s.append(a[i][j] * const)
-        draw_matrix()
+        length = len(a)
+        s = [[0 for j in range(length)] for i in range(length)]
+        for i in range(length):
+            for j in range(length):
+                s[i][j] = (a[i][j] * const)
+        for elem in s:
+            print(elem)
     if calculate == calculate_list[2]:
         n, m = map(int, input("Enter the number of lines and columns\n> ").split())
         a.clear()
@@ -119,7 +72,8 @@ while calculate != calculate_list[3]:
                 for j in range(length):
                     for k in range(length):
                         s[i][j] += a[i][k] * b[k][j]
-            draw_matrix_v2()
+            for elem in s:
+                print(elem)
         else:
             print("The operation cannot be performed")
     if calculate == calculate_list[4]:
