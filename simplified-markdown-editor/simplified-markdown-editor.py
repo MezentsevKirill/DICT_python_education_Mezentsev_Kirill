83668ad2cbe43c2a0067723dccc23a263f9625c4
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2022-02-17 14:04:50 +0200

    simplified-markdown-editor 4-th stage

--- simplified-markdown-editor/simplified-markdown-editor.py
+++ simplified-markdown-editor/simplified-markdown-editor.py
@@ -1,21 +1,25 @@
 command_list = ["plain", "bold/italic", "inline-code", "header", "link", "new-line",
                 "ordered-list", "unordered-list", "!help", "!done"]
 main_text = []
+main_text2 = []
 
 
 def plain():
     user_str = input("Text: >")
-    main_text.append(user_str)
+    main_text.append(user_str + "\n")
+    main_text2.append(user_str)
 
 
 def bold_italic():
     user_str = input("Text: >")
-    main_text.append(f"***{user_str}***")
+    main_text.append(f"***{user_str}***\n")
+    main_text2.append(f"***{user_str}***")
 
 
 def inline_code():
     user_str = input("Text: >")
-    main_text.append(f"`{user_str}`")
+    main_text.append(f"`{user_str}`\n")
+    main_text2.append(f"`{user_str}`")
 
 
 def header():
@@ -25,6 +29,7 @@ def header():
     else:
         user_str = input("Text: >")
         main_text.append("\n" + "#" * level + " " + user_str + "\n")
+        main_text2.append("\n" + "#" * level + " " + user_str + "\n")
 
 
 def new_line():
@@ -34,7 +39,8 @@ def new_line():
 def link():
     label = input("Label: >")
     url = input("URL: >")
-    main_text.append(f"[{label}]({url})")
+    main_text.append(f"[{label}]({url})\n")
+    main_text2.append(f"[{label}]({url})")
 
 
 def ordered_list():
@@ -44,7 +50,8 @@ def ordered_list():
     else:
         for i in range(1, rows + 1):
             user_text = input(f"Row #{i}:>")
-            main_text.append(f"{i}.{user_text}")
+            main_text.append(f"{i}.{user_text}\n")
+            main_text2.append(f"{i}.{user_text}")
 
 
 def unordered_list():
@@ -54,7 +61,15 @@ def unordered_list():
     else:
         for i in range(1, rows + 1):
             user_text = input(f"Row #{i}:>")
-            main_text.append(f"*{user_text}")
+            main_text.append(f"*{user_text}\n")
+            main_text2.append(f"*{user_text}")
+
+
+def write_to_file():
+    file = open("output.md", "w", encoding="utf-8")
+    for line in main_text:
+        file.write(line)
+    file.close()
 
 
 user_formatter = None
@@ -76,10 +91,13 @@ while user_formatter != command_list[9]:
         ordered_list()
     elif user_formatter == command_list[7]:
         unordered_list()
-    elif user_formatter not in command_list:
+    for lines in main_text2:
+        print(lines)
+    if user_formatter not in command_list:
         print("Unknown formatting type or command")
     elif user_formatter == command_list[8]:
-        print("Available formatters: plain, bold, italic, header, link, inline-code,"
+        print("Available formatters: plain, bold/italic, header, link, inline-code,"
               "ordered-list, unordered-list, new-line\nSpecial commands: !help !done")
-    for lines in main_text:
-        print(lines)
+
+
+write_to_file()
