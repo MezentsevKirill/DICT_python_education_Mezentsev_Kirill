7e25cd04e8aed5aeea26bc53534bd5274e3e5a2a
MezentsevKirill <k.v.mezentsev@student.khai.edu>
2022-02-17 13:12:22 +0200

    simplified-markdown-editor 2-nd stage

--- simplified-markdown-editor/simplified-markdown-editor.py
+++ simplified-markdown-editor/simplified-markdown-editor.py
@@ -1,16 +1,61 @@
-command_list = ["plain", "bold", "italic", "header", "link", "inline-code",
-                "ordered-list", "unordered-list", "new-line", "!help", "!done"]
+command_list = ["plain", "bold/italic", "inline-code", "header", "link", "new-line",
+                "ordered-list", "unordered-list", "!help", "!done"]
+main_text = []
 
 
-def choose_a_formatter():
+def plain():
+    user_str = input("Text: >")
+    main_text.append(user_str)
+
+
+def bold_italic():
+    user_str = input("Text: >")
+    main_text.append(f"***{user_str}***")
+
+
+def inline_code():
+    user_str = input("Text: >")
+    main_text.append(f"`{user_str}`")
+
+
+def header():
+    level = int(input("Level: >"))
+    if level < 1 or level > 6:
+        print("The level should be within the range of 1 to 6")
+    else:
+        user_str = input("Text: >")
+        main_text.append("\n" + "#" * level + " " + user_str + "\n")
+
+
+def new_line():
+    main_text[-1] = main_text[-1] + " " * 2
+
+
+def link():
+    label = input("Label: >")
+    url = input("URL: >")
+    main_text.append(f"[{label}]({url})")
+
+
+user_formatter = None
+while user_formatter != command_list[9]:
     user_formatter = input("Choose a formatter: > ")
-    if user_formatter not in command_list:
+    if user_formatter == command_list[0]:
+        plain()
+    elif user_formatter == command_list[1]:
+        bold_italic()
+    elif user_formatter == command_list[2]:
+        inline_code()
+    elif user_formatter == command_list[3]:
+        header()
+    elif user_formatter == command_list[4]:
+        link()
+    elif user_formatter == command_list[5]:
+        new_line()
+    elif user_formatter not in command_list:
         print("Unknown formatting type or command")
-    elif user_formatter == command_list[9]:
+    elif user_formatter == command_list[8]:
         print("Available formatters: plain, bold, italic, header, link, inline-code,"
-              "ordered-list, unordered-list, new-line")
-    elif user_formatter == command_list[10]:
-        raise SystemExit
-
-
-choose_a_formatter()
+              "ordered-list, unordered-list, new-line\nSpecial commands: !help !done")
+    for lines in main_text:
+        print(lines)
